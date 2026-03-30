6a7d6b17c67aea0e75a0988812dfc62bbf6aec12
Ivaylo Demirov <ivodemirov@gmail.com>
2026-03-30 21:16:27 +0300

    Initial copy from Basics project for Django Advanced

--- struct_portfolio_advanced/asgi.py
+++ struct_portfolio_advanced/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for struct_portfolio_uk project.
+ASGI config for struct_portfolio_advanced project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'struct_portfolio_uk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'struct_portfolio_advanced.settings')
 
 application = get_asgi_application()
--- struct_portfolio_advanced/urls.py
+++ struct_portfolio_advanced/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for struct_portfolio_uk project.
+URL configuration for struct_portfolio_advanced project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
